7ab06e847d14014ceedd95474d24856d56548196
Victor Hugo <victorze@protonmail.com>
2022-08-25 20:07:18 -0500

    Add another way to handle errors

--- src/main/java/com/victorze/stock/controllers/ProductController.java
+++ src/main/java/com/victorze/stock/controllers/ProductController.java
@@ -1,5 +1,19 @@
 package com.victorze.stock.controllers;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
 import com.victorze.stock.dto.CreateProductDTO;
 import com.victorze.stock.dto.ProductDTO;
 import com.victorze.stock.dto.converter.ProductDTOConverter;
@@ -8,13 +22,8 @@ import com.victorze.stock.models.Category;
 import com.victorze.stock.models.Product;
 import com.victorze.stock.repositories.CategoryRepository;
 import com.victorze.stock.repositories.ProductRepository;
-import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import lombok.AllArgsConstructor;
 
 @RestController
 @AllArgsConstructor
@@ -29,7 +38,7 @@ public class ProductController {
         List<Product> result = productRepository.findAll();
 
         if (result.isEmpty()) {
-            return ResponseEntity.notFound().build();
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No hay productos registrados");
         } else {
             List<ProductDTO> dtoList =
                     result.stream()
@@ -41,8 +50,12 @@ public class ProductController {
 
     @GetMapping("/products/{id}")
     public Product getProduct(@PathVariable Long id) {
-        return productRepository.findById(id)
-                .orElseThrow(() -> new ProductNotFoundException(id));
+        try {
+            return productRepository.findById(id)
+                    .orElseThrow(() -> new ProductNotFoundException(id));
+        } catch (ProductNotFoundException ex) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, ex.getMessage());
+        }
     }
 
     @PostMapping("/products")
--- src/main/java/com/victorze/stock/dto/converter/ProductDTOConverter.java
+++ src/main/java/com/victorze/stock/dto/converter/ProductDTOConverter.java
@@ -1,8 +1,6 @@
 package com.victorze.stock.dto.converter;
 
-import com.victorze.stock.dto.CreateProductDTO;
 import com.victorze.stock.dto.ProductDTO;
-import com.victorze.stock.models.Category;
 import com.victorze.stock.models.Product;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
